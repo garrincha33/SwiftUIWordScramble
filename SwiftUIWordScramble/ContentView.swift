d94e86be8f76d675df28ca4447d1981a6c3b3964
Richard Price <richyprice77@gmail.com>
2022-01-31 15:02:55 +0000

    inital commit

---
+++ SwiftUIWordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWordScramble
+//
+//  Created by Richard Price on 31/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWordScramble/SwiftUIWordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWordScrambleApp.swift
+//  SwiftUIWordScramble
+//
+//  Created by Richard Price on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
