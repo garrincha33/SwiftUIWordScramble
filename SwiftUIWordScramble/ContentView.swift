d59736fa512ce59cd18826883f5e4e2fbca9f14a
Richard Price <richyprice77@gmail.com>
2022-02-02 11:16:59 +0000

    added word validations and score calculation, also can restart for a new game

--- SwiftUIWordScramble/ContentView.swift
+++ SwiftUIWordScramble/ContentView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
+struct ScoreFont: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.largeTitle))
+            .foregroundColor(.red)
+    }
+}
+
+extension View {
+    func CustomFont() -> some View {
+        modifier(ScoreFont())
+    }
+}
+
 struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+ 
+
     
     var body: some View {
         NavigationView {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord).autocapitalization(.none)
+                    Text("Your score is \(score)")
+                        .CustomFont()
+                    
+                    
                 }
                 
                 Section {
@@ -30,20 +55,48 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
+            .toolbar {
+                Button("Restart") {
+                    startGame()
+                }
+            }
             .onSubmit {
                     addNewWord()
                 }
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
+            
         }
         
     }
     
     func addNewWord() {
+
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
+        
+        //validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "This word has been used already", message: "please try another")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "word not possible", message: "you cant spell that from \(rootWord)")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "word not recognised", message: "you cant just make them up you know!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
+        score += 1
         newWord = ""
     }
     
@@ -59,6 +112,7 @@ struct ContentView: View {
                 rootWord = allWords.randomElement() ?? "silkworm"
 
                 // If we are here everything has worked, so we can exit
+ 
                 return
             }
         }
@@ -66,7 +120,45 @@ struct ContentView: View {
         // If were are *here* then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
     }
+    
+    //MARK:- functions for game functionailty
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
